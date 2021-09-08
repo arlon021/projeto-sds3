c3fa20e0193deeda8b6af8964927ec21c614bc1a
Arlon Antonio <arlon20092009@gmail.com>
2021-09-07 21:10:56 -0300

    Basic static components

---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,36 @@
+function Footer() {
+  return (
+    <div>
+      <footer className="footer mt-auto py-3 bg-dark">
+        <div className="container">
+          <p className="text-light">
+            App desenvolvido por{' '}
+            <a
+              href="https://github.com/arlon021"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Arlon Antonio
+            </a>
+          </p>
+          <p className="text-light">
+            <small>
+              <strong>Semana Spring React</strong>
+              <br />
+              Evento promovido pela escola DevSuperior:{' '}
+              <a
+                href="https://instagram.com/devsuperior.ig"
+                target="_blank"
+                rel="noreferrer"
+              >
+                @devsuperior.ig
+              </a>
+            </small>
+          </p>
+        </div>
+      </footer>
+    </div>
+  );
+}
+
+export default Footer;
